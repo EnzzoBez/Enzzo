20b50390a9ee0f1e5478f97222925b0bb645ae61
Enzzo Bez <151688241+EnzzoBez@users.noreply.github.com>
2024-03-13 23:28:23 +0000

    Ex01

--- Tdsprograma/Ex01.java
+++ Tdsprograma/Ex01.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class  Ex01{
     public static void main(String[] args){
-        Scanner ler = new Scanner(System.in){
-            float idade = ler.nextInt();
-            int nome;
-            
-        }]
-        
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Insira seu nome: ");
+        String nome = ler.next();
+        System.out.println("Insira sua idade: ");
+        int idade = ler.nextInt();
+
+        System.out.println(nome);
+        System.out.println(idade);
     } 
-}
 }
