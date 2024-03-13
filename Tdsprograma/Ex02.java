b21f9d2bf270bbe1acdb553f811843f3dddf71c2
Enzzo Bez <151688241+EnzzoBez@users.noreply.github.com>
2024-03-13 23:50:16 +0000

    Calculo N inteiro

--- Tdsprograma/Ex02.java
+++ Tdsprograma/Ex02.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Ex02{
     public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
 
+        System.out.println("Insira o primeiro número: ");
+        double numero1 = ler.nextDouble();
+        System.out.println("Insira o segundo Número: ");
+        double numero2 =  ler.nextDouble();
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("O resultado da soma é: " + resultado);
     }
 }
