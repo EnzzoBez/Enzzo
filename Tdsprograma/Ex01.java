327e4d21eea9ab7c54689281f3406aae958c16ef
Enzzo Bez <151688241+EnzzoBez@users.noreply.github.com>
2024-03-13 22:40:18 +0000

    Entrada e saida de dados

---
+++ Tdsprograma/Ex01.java
@@ -0,0 +1,11 @@
+public class  Ex01{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in){
+            float idade = ler.nextInt();
+            int nome;
+            
+        }]
+        
+    } 
+}
+}
